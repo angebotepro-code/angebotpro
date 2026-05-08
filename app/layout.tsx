be230efd8e18e5718391b34717895d5bfc78ca24
AngebotPro <angebotepro@gmail.com>
2026-05-08 18:14:58 +0200

    Fix shadcn theme: enable dark class + emerald primary colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
+      className={`${inter.variable} ${jetbrainsMono.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
